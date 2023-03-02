769d22a71568f7e180161a72cdd83967ab69229f
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-02 20:39:49 +0100

    iteration2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
 import productData from './products.json';
+import ProductCard from './components/ProductCard'
 
 function App() {
   const [products, setProducts] = useState(productData);
 
+  const handleDelete =(id)=>{
+    const cleanProducts = [...products].filter(elem => elem._id !== id)
+    setProducts(cleanProducts)
+  }
   return (
     <>
       <h1>My shopping cart</h1>
       <div className="cart">
-        {/* ITERATION 1 */}
-
+       {products.map(elem =>{
+        return <ProductCard product={elem} key={elem.id} handleDelete={handleDelete} />
+       })}
       </div>
     </>
   );
