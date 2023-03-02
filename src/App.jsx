dc708b52db6302fb573cfef42cecbd4badf15555
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-02 21:12:37 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
-import React, { useState } from 'react';
-import './App.css';
-import productData from './products.json';
-import ProductCard from './components/ProductCard';
-import SearchInput from './components/SearchInput';
+import React, { useState } from "react";
+import "./App.css";
+import productData from "./products.json";
+import ProductCard from "./components/ProductCard";
+import SearchInput from "./components/SearchInput";
+import AddProductForm from "./components/AddProduct";
 
 function App() {
   const [products, setProducts] = useState(productData);
-  const[searchValue, setSearchValue] = useState('');
-  const handleDelete =(id)=>{
-    const cleanProducts = [...products].filter(elem => elem._id !== id)
-    setProducts(cleanProducts)
-  }
-  
-  const handleSearch =(string)=>{
-      setSearchValue(string)
-  }
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleDelete = (id) => {
+    const cleanProducts = [...products].filter((elem) => elem._id !== id);
+    setProducts(cleanProducts);
+  };
+  const handleNewProduct = (obj) => {
+    const product = { ...obj, _id: new Date().toString() };
+    setProducts([...products, product]);
+  };
+
+  const handleSearch = (string) => {
+    setSearchValue(string);
+  };
 
   return (
     <>
       <h1>My shopping cart</h1>
-      <SearchInput handleSearch={handleSearch}/>
+      <SearchInput handleSearch={handleSearch} />
+      <AddProductForm handleNewProduct={handleNewProduct} />
       <div className="cart">
-       {products.filter(elem=> elem.name.toLowerCase().includes(searchValue)).map(elem =>{
-        return <ProductCard product={elem} key={elem.id} handleDelete={handleDelete} />
-       })}
+        {products
+          .filter((elem) => elem.name.toLowerCase().includes(searchValue))
+          .map((elem) => {
+            return (
+              <ProductCard
+                product={elem}
+                key={elem.id}
+                handleDelete={handleDelete}
+              />
+            );
+          })}
       </div>
     </>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function ProductCard({ product ,handleDelete }) {
-
+export default function ProductCard({ product, handleDelete }) {
   const { name, price, image, _id } = product;
 
-
   const handleProductDelete = () => {
-    handleDelete(_id)
-  }
+    handleDelete(_id);
+  };
 
   return (
     <div className="product_card">
       <h3>{name}</h3>
       <img src={image} alt={name} />
       <p>Price: {price}$</p>
-      <button className="btn_delete" onClick={handleProductDelete}>Delete</button>
+      <button className="btn_delete" onClick={handleProductDelete}>
+        Delete
+      </button>
     </div>
-  )
+  );
 }
